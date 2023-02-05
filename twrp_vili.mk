d30a5b2554ca4c326d6757b8d714ecb0c70d1cad
Farrukh Seyer Hasan <59789349+farrukh2002@users.noreply.github.com>
2023-02-06 03:14:17 +0530

    Update twrp_vili.mk

--- twrp_vili.mk
+++ twrp_vili.mk
@@ -22,16 +22,10 @@ $(call inherit-product, device/xiaomi/vili/device.mk)
 # Inherit from PBRP-common stuffs.
 $(call inherit-product, vendor/pb/config/common.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := vili
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := vili
-PRODUCT_RELEASE_NAME := vili
-PRODUCT_NAME := twrp_vili
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Xiaomi 11T Pro
-PRODUCT_MANUFACTURER := Xiaomi
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := $(PRODUCT_RELEASE_NAME)
@@ -40,3 +34,11 @@ TARGET_OTA_ASSERT_DEVICE := $(PRODUCT_RELEASE_NAME)
 TW_STATUS_ICONS_ALIGN := center
 TW_Y_OFFSET := 90
 TW_H_OFFSET := -90
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := vili
+PRODUCT_RELEASE_NAME := vili
+PRODUCT_NAME := twrp_vili
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Xiaomi 11T Pro
+PRODUCT_MANUFACTURER := Xiaomi
