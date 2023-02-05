c52a5c446194f42544c74149777642c50c575031
Farrukh Seyer Hasan <59789349+farrukh2002@users.noreply.github.com>
2023-02-06 03:36:06 +0530

    Update twrp_vili.mk

--- twrp_vili.mk
+++ twrp_vili.mk
@@ -20,12 +20,21 @@ DEVICE_PATH := device/xiaomi/vili
 $(call inherit-product, device/xiaomi/vili/device.mk)
 
 # Inherit from PBRP-common stuffs.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/pb/config/Android.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := vili
+PRODUCT_RELEASE_NAME := vili
+PRODUCT_NAME := twrp_vili
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Xiaomi 11T Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := $(PRODUCT_RELEASE_NAME)
@@ -35,10 +44,3 @@ TW_STATUS_ICONS_ALIGN := center
 TW_Y_OFFSET := 90
 TW_H_OFFSET := -90
 
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := vili
-PRODUCT_RELEASE_NAME := vili
-PRODUCT_NAME := twrp_vili
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Xiaomi 11T Pro
-PRODUCT_MANUFACTURER := Xiaomi
